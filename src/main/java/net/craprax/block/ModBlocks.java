17c92b6bb04bd1fa6e5c8b1e7763dcc1e8015868
Craprax <dennykunkel99@gmail.com>
2023-07-08 17:32:02 +0200

    added phoenixit_ore

--- src/main/java/net/craprax/block/ModBlocks.java
+++ src/main/java/net/craprax/block/ModBlocks.java
@@ -1,8 +1,12 @@
 package net.craprax.block;
 
 import net.craprax.ExoticCrystals;
+import net.craprax.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,6 +14,12 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
+
+    public static final Block PHOENIXIT_ORE = registerBlock("phoenixit_ore",
+            new OreBlock(FabricBlockSettings.of(Material.METAL)
+                    .strength(1f)
+                    .requiresTool()),
+            ModItemGroup.EXOTIC_CRYSTALS);
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
         return Registry.register(Registry.BLOCK, new Identifier(ExoticCrystals.MOD_ID, name), block);
