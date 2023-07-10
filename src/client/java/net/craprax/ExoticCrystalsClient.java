8240508fb9ef4e16e49039e01efd4939e55ab4dd
Craprax <dennykunkel99@gmail.com>
2023-07-10 23:00:47 +0200

    added crafting recipes for crystals and phoenixit bow

--- src/client/java/net/craprax/ExoticCrystalsClient.java
+++ src/client/java/net/craprax/ExoticCrystalsClient.java
@@ -1,10 +1,11 @@
 package net.craprax;
 
+import net.craprax.util.ModModelPredicateProvider;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ExoticCrystalsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ModModelPredicateProvider.registerModModels();
 	}
 }
--- src/main/java/net/craprax/item/ModItems.java
+++ src/main/java/net/craprax/item/ModItems.java
@@ -2,12 +2,18 @@ package net.craprax.item;
 
 import net.craprax.ExoticCrystals;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item ROOTICIUM_CRYSTAL = registerItem("rooticium_crystal",
+            new Item(new FabricItemSettings().group(ModItemGroup.EXOTIC_CRYSTALS)));
+
+    public static final Item PHOENIXIT_CRYSTAL = registerItem("phoenixit_crystal",
             new Item(new FabricItemSettings().group(ModItemGroup.EXOTIC_CRYSTALS)));
 
     public static final Item ROOTICIUM_SHARD = registerItem("rooticium_shard",
@@ -19,6 +25,8 @@ public class ModItems {
     public static final Item DELTARIUM_SHARD = registerItem("deltarium_shard",
             new Item(new FabricItemSettings().group(ModItemGroup.EXOTIC_CRYSTALS)));
 
+    public static final Item PHOENIXIT_BOW = registerItem("phoenixit_bow",
+            new BowItem(new FabricItemSettings().group(ItemGroup.COMBAT).maxDamage(8000).rarity(Rarity.EPIC)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ExoticCrystals.MOD_ID, name), item);
